47b4e664df43d5a36cd92e71806f2dd9f535d74c
maharaghu1 <mahaviswana@gmail.com>
2025-07-09 18:59:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import homepage
+import streamlit as st
 
-homepage.app()
+def app():
+    st.image("https://8e41335d61f2.ngrok-free.app/assets/logo.png", width=160)
+    st.title("Welcome to WasteHawk")
+    st.markdown("AI-powered drone surveillance for illegal dumping.")
